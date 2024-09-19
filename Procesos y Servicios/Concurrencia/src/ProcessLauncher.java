50b6b47559f2867dd48c334e5e9ca2c8128cf7f0
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-09-19 12:12:41 +0200

    a

--- Procesos y Servicios/Concurrencia/src/ProcessLauncher.java
+++ Procesos y Servicios/Concurrencia/src/ProcessLauncher.java
@@ -1,3 +1,4 @@
+import java.io.IOError;
 import java.util.Scanner;
 
 public class ProcessLauncher {
@@ -6,23 +7,34 @@ public class ProcessLauncher {
         try {
             ProcessBuilder pb = new ProcessBuilder("kcalc");
             Process p = pb.start();
-            String respuesta;
+            
+            boolean cont = true;
             do {
                 System.out.println("Desea finalizar el proceso? (Y para si, N para no)");
-                respuesta = sc.nextLine();
+                String respuesta = sc.nextLine();
                 if (respuesta.equalsIgnoreCase("y")) {
+                    cont=false;
+                    System.out.println("Proceso matado:");
                     System.out.println("PID: " + p.pid());
                     System.out.println("INFO: " + p.info());
                     p.destroy();
                 } else if (respuesta.equalsIgnoreCase("n")) {
-                    Thread.sleep(10000);
+                    cont = false;
+                    System.out.println("Esperamos a que el proceso muera");
                 } else{
                     System.out.println("Letra mal introducida.");
                     Thread.sleep(2000);
                 }
             } while (!respuesta.equalsIgnoreCase("y"));
-            System.out.println("Final");
-        } catch (Exception e) {
+
+            int result = p.waitFor();
+            int result2 = p.exitValue();
+            System.out.println(result == result2);
+            System.out.println("Final proceso (" + p.pid() + "). Resultado: " + result + ". Información adicional: " + p.info());
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(InterruptedException e){
             e.printStackTrace();
         }
     }
--- Procesos y Servicios/Concurrencia/src/Tarea.java
+++
@@ -1,8 +0,0 @@
-public class Tarea extends Thread{
-    @Override
-    public void run() {
-        for(int i = 0; i < 500; i++){
-            System.out.println(i);
-        }
-    }
-}
