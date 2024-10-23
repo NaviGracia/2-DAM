7e8210ef9d9bd2b75aafce17dc67daa1fd7c229c
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-10-23 12:16:37 +0200

    v0.1

--- Multimedia y Móviles/IMCApp/app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ Multimedia y Móviles/IMCApp/app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -1,12 +1,56 @@
 package com.example.imcapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 
 class ImcCalculatorActivity : AppCompatActivity() {
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    private lateinit var rsHeight:Slider
+    private lateinit var tvHeight:TextView
+    private var cardSelected:Boolean = false
+
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners(){
+        viewMale.setOnClickListener{
+            cardSelected = true
+            setGenderColor()
+            cardSelected = false
+        }
+        viewFemale.setOnClickListener{
+            cardSelected = true
+            setGenderColor()
+            cardSelected = false
+        }
+    }
+
+    private fun setGenderColor(){
+        viewMale.setCardBackgroundColor(getBackgroundColor(cardSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(cardSelected))
+    }
+
+    private fun getBackgroundColor(isComponentSelected: Boolean):Int{
+        val colorReference = if(isComponentSelected){
+            R.color.bg_comp_sel
+        }else{R.color.bg_comp}
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI(){
+        setGenderColor()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +60,9 @@ class ImcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initComponents()
+        initListeners()
+        initUI()
     }
+
 }
