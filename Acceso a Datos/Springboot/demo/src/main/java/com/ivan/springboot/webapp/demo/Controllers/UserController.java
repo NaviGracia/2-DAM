e3572f42aa10c9d12b05fabdd4efcb05eab7f3e6
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-11-08 13:10:10 +0100

    fdss

---
+++ Acceso a Datos/Springboot/demo/src/main/java/com/ivan/springboot/webapp/demo/Controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.ivan.springboot.webapp.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/details") 
+    public String mostrar(){
+        return "hola";
+    }
+}
