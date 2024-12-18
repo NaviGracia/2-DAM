fea5f27712e984f0d84e3bc20b10015ebe1edf65
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-12-18 13:58:25 +0100

    v0.1 tienda

---
+++ Acceso a Datos/Springboot/tiendas/src/test/java/com/ivangarcia/springboot/actividadfinal/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivangarcia.springboot.actividadfinal.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
